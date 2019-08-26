5d85b8bb79f3cc96eebc15c39b79a22f36ae8f29
yaelBrown <yaelrbrown@gmail.com>
2019-08-26 13:36:32 -0500

    not able to get components to work

---
+++ codeupreact/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+const navbar = () => {
+  render = () => {
+    return (
+      <h1>This is navbar</h1>
+    )
+  }
+}
+
+export default navbar;
---
+++ codeupreact/src/components/rebootYourCareer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom'
+
+const rebootYourCareer = () => {
+  render = () => {
+    return ( 
+        <div>
+          reboot your career
+        </div>
+      )
+    }
+}
+
+export default rebootYourCareer;
