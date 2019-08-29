4e8e01491bb7ad26411e3b50f3e45eda811ec4b7
Yael Brown <yaelrbrown@gmail.com>
2019-08-28 21:32:20 -0500

    worked on programs page and reboot

--- codeupreact/src/components/Programs.js
+++ codeupreact/src/components/Programs.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
-import rebootPic1 from './img/rebootPic1.png';
 
 export default class Programs extends Component {
   render() {
     return (
-      <section className="reboot">
-        {/* Left section */}
-        <div className="rebootLeftSide bborder">
-          <h1>Reboot Your Career<span>_</span></h1>
-        </div>
-
-        {/* Right side */}
-        <div className="rebootRightSide bborder">
-          <img src={rebootPic1} alt="Right Side image in Reboot Section" />
-        </div>
+      <section>
+        <h1>Programs</h1>
       </section>
     )
   }
--- codeupreact/src/components/Reboot.js
+++ codeupreact/src/components/Reboot.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import rebootPic1 from './img/rebootPic1.png';
 
 export default class Reboot extends Component {
   render() {
     return (
-      <div>
-        <h1>Reboot your Career</h1>
-      </div>
+      <section className="reboot">
+        {/* Left section */}
+        <div className="rebootLeftSide">
+          {/* chevron */}
+          <h1>Reboot Your Career<span className="underscore">_</span></h1>
+          <p>Codeup offers 20-week accelerator programs to launch your career as a computer programmer or a data scientist.</p>
+          {/* Learn more button */}
+        </div>
+
+        {/* Right side */}
+        <div className="rebootRightSide">
+          <img src={rebootPic1} alt="Right Side image in Reboot Section" />
+        </div>
+      </section>
     )
   }
 }
