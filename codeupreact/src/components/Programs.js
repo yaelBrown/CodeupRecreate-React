efdd5c0ecc0a743c17510925076e135e528863de
yaelBrown <yaelrbrown@gmail.com>
2019-08-29 08:43:39 -0500

    added changes to programs.js file.

--- codeupreact/src/components/Programs.js
+++ codeupreact/src/components/Programs.js
@@ -6,12 +6,12 @@ export default class Programs extends Component {
     return (
       <section className="reboot">
         {/* Left section */}
-        <div className="rebootLeftSide bborder">
+        <div className="rebootLeftSide">
           <h1>Reboot Your Career<span>_</span></h1>
         </div>
 
         {/* Right side */}
-        <div className="rebootRightSide bborder">
+        <div className="rebootRightSide">
           <img src={rebootPic1} alt="Right Side image in Reboot Section" />
         </div>
       </section>
